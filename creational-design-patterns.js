c0244c39463ae204d90f4ef5e81d8f9dd72ef742
Ralph Effting <ralfting@gmail.com>
2017-08-06 11:32:48 -0300

    Implement singleton

--- creational-design-patterns.js
+++ creational-design-patterns.js
@@ -1,40 +1,20 @@
 (function(win, $) {
   'use strict';
 
-  var RedCircle = function() {
-    this.item = $('<div class="circle -red"></div>');
-  };
-  var BlueCircle = function() {
-    this.item = $('<div class="circle -blue"></div>');
-  };
-
-  // Factory
-  var CircleFactory = function() {
-    this.create = function(color) {
-      if (color === "red") {
-        return new RedCircle();
-      } else {
-        return new BlueCircle();
-      }
-    };
-  };
-
-  // Singleton
   var CircleGeneratorSingleton = (function() {
     var instance;
 
     function init() {
       var _aCircle = [],
-          _state = $('body'),
-          _circleFactory = new CircleFactory();
+          _state = $('body');
 
       function _position(circle, left, top) {
         circle.css('left', left);
         circle.css('top', top);
       }
 
-      function create(left, top, color) {
-        var circle = _circleFactory.create(color).item;
+      function create(left, top) {
+        var circle = $('<div class="circle"></div>');
         _position(circle, left, top);
 
         return circle;
@@ -68,37 +48,38 @@
   }());
 
   $(win.document).ready(function(){
-    // click
     $('body').click(function(e) {
       var cgInstance = CircleGeneratorSingleton.getInstance();
-      var circle = cgInstance.create(e.pageX-25, e.pageY-25, "red");
+      var circle = cgInstance.create(e.pageX-25, e.pageY-25);
       cgInstance.add(circle);
     });
 
-    // keypress
     $(document).keypress(function(e) {
       if(e.key === 'a') {
         var cgInstance = CircleGeneratorSingleton.getInstance();
         var circle = cgInstance.create(Math.floor(Math.random() * e.target.clientWidth),
-                                       Math.floor(Math.random() * e.target.clientHeight), "blue");
+                                        Math.floor(Math.random() * e.target.clientHeight));
         cgInstance.add(circle);
       }
     });
   });
 }(window, jQuery));
 
-// // OLD
+
+
+
+// OLD
 // $(window.document).ready(function(){
 //   "use strict";
 //
 //   $('html').click(function(e) {
 //    // Creation
-//       var circle = $('<div class="cirle"></div>');
-//
-//       // position
-//       circle.css('left', e.pageX-25);
-//       circle.css('top', e.pageY-25);
-//
-//       $('html').append(circle);
+      // var circle = $('<div class="cirle"></div>');
+      //
+      // // position
+      // circle.css('left', e.pageX-25);
+      // circle.css('top', e.pageY-25);
+      //
+      // $('html').append(circle);
 //   });
 // });
